361b235d3388c09c248d21ad0df23f36333212da
Tarbolin69 <nicolas@solcaronte.xyz>
2024-10-07 14:16:32 -0300

    Grafo adyacencia terminado

--- src/Grafos/Nodo.java
+++ src/Grafos/Nodo.java
@@ -3,28 +3,28 @@ package Grafos;
 public class Nodo {
 
     /*
-    Provincia de Río Negro, Viedma
-    Provincia de Tierra del Fuego, Ushuaia
-    Provincia de Santiago del Estero, Santiago del Estero
-    Provincia de La Pampa, Santa Rosa
-    Provincia de Santa Fe, Santa Fe
-    Provincia de Jujuy, San Salvador de Jujuy
-    Provincia de Tucumán, San Miguel de Tucumán
-    Provincia de San Luis, San Luis
-    Provincia de San Juan, San Juan
-    Provincia de Catamarca, San Fernando del Valle de Catamarca
-    Provincia de Salta, Salta
-    Provincia de Santa Cruz, Río Gallegos
-    Provincia del Chaco, Resistencia
-    Provincia del Chubut, Rawson
-    Provincia de Misiones, Posadas
-    Provincia de Entre Ríos, Paraná
-    Provincia del Neuquén, Neuquén
-    Provincia de Mendoza, Mendoza
-    Provincia de La Rioja, La Rioja
-    Provincia de Buenos Aires, La Plata
-    Provincia de Formosa, Formosa
-    Provincia de Corrientes, Corrientes
-    Provincia de Córdoba, Córdoba
+    A Viedma
+    B Ushuaia
+    C Santiago del Estero
+    D Santa Rosa
+    E Santa Fe
+    F San Salvador de Jujuy
+    G San Miguel de Tucumán
+    H San Luis
+    I San Juan
+    J San Fernando del Valle de Catamarca
+    K Salta
+    L Río Gallegos
+    M Resistencia
+    N Rawson
+    O Posadas
+    P Paraná
+    Q Neuquén
+    R Mendoza
+    S La Rioja
+    T La Plata
+    U Formosa
+    V Corrientes
+    W Córdoba
      */
 }
