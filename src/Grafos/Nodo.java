d77271523735b077210a356607545d34189ad090
Tarbolin69 <tarbolin69@gmail.com>
2024-10-27 21:26:34 -0300

    nuevos nodos

--- src/Grafos/Nodo.java
+++ src/Grafos/Nodo.java
@@ -2,30 +2,30 @@ package Grafos;
 
 public class Nodo {
     /*
-    1 A Viedma
-    2 B Ushuaia
-    3 C Santiago del Estero
-    4 D Santa Rosa
-    5 E Santa Fe
-    6 F San Salvador de Jujuy
-    7 G San Miguel de Tucumán
-    8 H San Luis
-    9 I San Juan
-    10 J San Fernando del Valle de Catamarca
-    11 K Salta
-    12 L Río Gallegos
-    13 M Resistencia
-    14 N Rawson
-    15 O Posadas
-    16 P Paraná
-    17 Q Neuquén
-    18 R Mendoza
-    19 S La Rioja
-    20 T La Plata
-    21 U Formosa
-    22 V Corrientes
-    23 W Córdoba
-    24 X BSAS
+    1 A Viedma: {2, 8}, {4, 6}, {23, }, {24, }
+    2 B Ushuaia:
+    3 C Santiago del Estero:
+    4 D Santa Rosa:
+    5 E Santa Fe:
+    6 F San Salvador de Jujuy:
+    7 G San Miguel de Tucumán:
+    8 H San Luis:
+    9 I San Juan:
+    10 J San Fernando del Valle de Catamarca:
+    11 K Salta:
+    12 L Río Gallegos:
+    13 M Resistencia:
+    14 N Rawson:
+    15 O Posadas:
+    16 P Paraná:
+    17 Q Neuquén:
+    18 R Mendoza:
+    19 S La Rioja:
+    20 T La Plata:
+    21 U Formosa:
+    22 V Corrientes:
+    23 W Córdoba:
+    24 X BSAS:
      */
     private int vertice;
     private int aristaPeso;
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import Grafos.Grafo;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -26,6 +28,7 @@ public class Main {
            Objectivo: Calcular camino mas corto entre BSAS y X sin ir directamente a X
          */
         int[] CIUDADES = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
-        int
+        int GRAFO_LARGO = CIUDADES.length;
+        Grafo grafoArgentina = new Grafo(GRAFO_LARGO);
     }
 }
